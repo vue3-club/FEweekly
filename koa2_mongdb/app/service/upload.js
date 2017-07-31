bd67b9a680d73e18d4e197afeb1a85f63d5e64e2
febobo <584516601@qq.com>
2017-07-31 14:54:14 +0800

    add upload ctrl

---
+++ koa2_mongdb/app/service/upload.js
@@ -0,0 +1,10 @@
+import qn from 'qn'
+
+const client = qn.create({
+  accessKey: 'K1EzvixYD2GbvvsiX3ko6g989GuVMUZkyrCdUl5o',
+  secretKey: 'ggoFiqnCsE_Un4XpKPRqYQJyke8_Yo6kApm2bcsh',
+  bucket: 'vueclub',
+  domain: 'http://ooaa8syjw.bkt.clouddn.com',
+})
+
+export default client
