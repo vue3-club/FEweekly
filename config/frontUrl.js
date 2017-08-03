6166885e8483663296abeee48671182d86d5c0db
febobo <584516601@qq.com>
2017-08-03 11:15:24 +0800

    frontUrl setting

--- config/frontUrl.js
+++ config/frontUrl.js
@@ -1,1 +1,8 @@
-export const frontUrl = "http://api.feweekly.vue-js.com"
+const api = {
+	development: 'http://127.0.0.1:9090',
+	production: "http://api.feweekly.vue-js.com"
+}
+export const frontUrl = (()=>{
+	if(process.env.NODE_ENV) return api[process.env.NODE_ENV];
+	return api['development'];
+})()
