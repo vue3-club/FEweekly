70151226cf816a8966e477451c123ed5b69c287d
wuyanwen <wj.wuyanwen@le.com>
2017-08-24 10:52:47 +0800

    bug

--- server/config.default.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-	db: 'mongodb://localhost:10086/feweekly',
-	qn_accessKey: 'qn_accessKey',
-	qn_secretKey: 'qn_secretKey',
-	bucket: 'vueclub',
-	domain: 'http://ooaa8syjw.bkt.clouddn.com'
-}
