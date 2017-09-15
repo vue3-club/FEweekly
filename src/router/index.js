bb90509ce9685023341b829a63bdac2ba8c7aa74
febobo <584516601@qq.com>
2017-09-15 16:00:14 +0800

    support period get data

--- src/router/index.js
+++ src/router/index.js
@@ -12,8 +12,7 @@ import 'font-awesome/css/font-awesome.min.css'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Hello',
       components: {
@@ -23,44 +22,66 @@ export default new Router({
       hidden: true
     },
     {
+        path: '/period/:id',
+        name: 'Hello',
+        components: {
+          default: Hello,
+          footer: Footer
+        },
+        hidden: true
+      },
+    {
       path: '/admin',
       component: Login,
       name: '',
       hidden: true
     },
     {
-        path: '/404',
-        component: NotFound,
-        name: '',
-        hidden: true
+      path: '/404',
+      component: NotFound,
+      name: '',
+      hidden: true
     },
     {
-        path: '/admin',
-        component: Home,
-        name: '周刊分类',
-        iconCls: 'el-icon-message',//图标样式class
-        children: [
-            { path: 'index', component: Index,name: '周刊', hidden: true  },
-            { path: 'weeklyClassificate', component:WeeklyClassificate, name: '周刊分类'}
-            // { path: '/table', component: Table, name: 'Table' },
-            // { path: '/form', component: Form, name: 'Form' },
-            // { path: '/user', component: user, name: '列表' },
-        ]
+      path: '/admin',
+      component: Home,
+      name: '周刊分类',
+      iconCls: 'el-icon-message', //图标样式class
+      children: [{
+          path: 'index',
+          component: Index,
+          name: '周刊',
+          hidden: true
+        },
+        {
+          path: 'weeklyClassificate',
+          component: WeeklyClassificate,
+          name: '周刊分类'
+        }
+        // { path: '/table', component: Table, name: 'Table' },
+        // { path: '/form', component: Form, name: 'Form' },
+        // { path: '/user', component: user, name: '列表' },
+      ]
     },
     {
-        path: '/admin',
-        component: Home,
-        name: '周刊列表',
-        iconCls: 'fa fa-id-card-o',
-        children: [
-            { path: 'weeklylist', component: Weeklylist, name: '周刊列表' }
-            // { path: '/page5', component: Page5, name: '页面5' }
-        ]
+      path: '/admin',
+      component: Home,
+      name: '周刊列表',
+      iconCls: 'fa fa-id-card-o',
+      children: [{
+          path: 'weeklylist',
+          component: Weeklylist,
+          name: '周刊列表'
+        }
+        // { path: '/page5', component: Page5, name: '页面5' }
+      ]
     },
     {
-        path: '*',
-        hidden: true,
-        redirect: { path: '/404' }
+      path: '*',
+      hidden: true,
+      redirect: {
+        path: '/404'
+      }
     }
   ]
 })
